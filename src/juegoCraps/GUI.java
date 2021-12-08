9fba7303b0ee6f3b1dcf502ca2995574f4dd3cfc
MarlonAnacona <ejemplo@email.com>
2021-12-08 14:45:25 -0500

    finished GUI sketch 1

--- src/juegoCraps/GUI.java
+++ src/juegoCraps/GUI.java
@@ -1,19 +1,40 @@
 package juegoCraps;
 
 import javax.swing.*;
+import javax.swing.text.html.HTMLEditorKit;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
 public class GUI extends JFrame {
     //atributos
-
+    private static final String MENSAJE_INICIO="BIENVENIDO A CRAPS \n"
+            +"Oprime el boton lanzar para iniciar el juego"+
+            "\n si tu tiro de salida es 7 u 11 ganas con natural"
+            +"\nSi tu tio de salida es 2,3 u 12 pierdes con craps"
+            +"\nSi sacas cualquier otro valor estableceras punto"
+            +"\n Estado en punto podras seguir  lanzando dados"
+            +"\npero ahora ganarás si sacas  nuevamente el valor de punto"
+            +"\n sin que previamente hayas sacado 7";
+    private Header headerProject;
+    private JLabel dado1,dado2;
+    private  JButton lanzar;
+    private JPanel panelDados,panelResultados;
+    private ImageIcon imagendDado;
+    private  JTextArea mensajeSalida,resultadosDados;
+    private JSeparator separador;
+    private Escucha escucha;
+    private modelCraps modelCraps;
     //metodos
 
     public GUI() {
         intGUI();
         //configurar ventana
-        this.setTitle("Mi presentación");
-        this.setSize(600, 540);
+        this.setTitle("Juego Craps");
+        this.pack();
+        this.setResizable(true);
+        //this.setSize(600, 540);
         this.setVisible(true);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -21,19 +42,84 @@ public class GUI extends JFrame {
     }
 
     private void intGUI() {
+    //create listener object or control object
+        escucha= new Escucha();
+        modelCraps=new modelCraps();
+        //set up Jcomponents
+        headerProject= new Header("Mesa Juego craps",Color.black);
+        this.add(headerProject,BorderLayout.NORTH);
+
+
+        imagendDado= new ImageIcon(getClass().getResource("/resources/dado.png"));
+        dado1= new JLabel(imagendDado);
+        dado2= new JLabel(imagendDado);
+
+        lanzar =new JButton("lanzar");
+        lanzar.addActionListener(escucha);
+
+        panelDados= new JPanel();
+        panelDados.setPreferredSize(new Dimension(300,180));
+        panelDados.setBorder(BorderFactory.createTitledBorder("Tus dados "));
+
+        panelDados.add(dado1);
+        panelDados.add(dado2);
+        panelDados.add(lanzar);
+        this.add(panelDados,BorderLayout.CENTER);
+
+        mensajeSalida= new JTextArea(7,31);
+        mensajeSalida.setText(MENSAJE_INICIO);
+        //mensajeSalida.setBorder(BorderFactory.createTitledBorder("Que debes hacer "));
+        JScrollPane scrrol= new JScrollPane(mensajeSalida);
+
+        panelResultados=new JPanel();
+        panelResultados.setBorder(BorderFactory.createTitledBorder("Que debes hacer "));
+        panelResultados.add(scrrol);
+        panelResultados.setPreferredSize(new Dimension(370,180));
+        this.add(panelResultados ,BorderLayout.EAST);
+        resultadosDados= new JTextArea(4,31);
+        separador= new JSeparator();
+        separador.setPreferredSize(new Dimension(320,7));
+        separador.setBackground(Color.BLUE);
+
     }
 
 
     public static void main(String[] args) {
         // Ejecución del programa
 
-        EventQueue.invokeLater(new Runnable() {
+        EventQueue.invokeLater(()-> {
 
-            public void run() {
 
                 GUI presentacion = new GUI();
-            }
+
 
         });
+
+    }
+
+    private class Escucha implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            modelCraps.calcularTiro();
+            int[] caras= modelCraps.getCaras();
+            imagendDado= new ImageIcon(getClass().getResource("/resources/"+caras[0]+".png"));
+            dado1.setIcon(imagendDado);
+            imagendDado= new ImageIcon(getClass().getResource("/resources/"+caras[1]+".png"));
+            dado2.setIcon(imagendDado);
+            modelCraps.determinarJuego();
+
+            panelResultados.removeAll();
+            panelResultados.setBorder(BorderFactory.createTitledBorder("Resultados "));
+            panelResultados.add(resultadosDados);
+            panelResultados.add(separador);
+            panelResultados.add(mensajeSalida);
+            resultadosDados.setText(modelCraps.getEstadoToString()[0]);
+            mensajeSalida.setRows(4);
+            mensajeSalida.setText(modelCraps.getEstadoToString()[1]);
+        revalidate();
+        repaint();
+
+        }
     }
 }
--- src/juegoCraps/modelCraps.java
+++ src/juegoCraps/modelCraps.java
@@ -1,10 +1,22 @@
 package juegoCraps;
 
+import java.util.SimpleTimeZone;
+
+/**
+ * Class modelCraps apply craps rules
+ *
+ * estado=1 natural winner
+ * estado=2 craps looser
+ * estado=3 estabilish punto
+ * estado=4 punto winner
+ * estado=5 punto looser
+ *
+ */
 public class modelCraps {
 
     private Dado dado1,dado2;
     private int tiro, punto,estado,flag;
-    private String estadoToString ;
+    private String[] estadoToString ;
     private int[] caras;
 
 
@@ -13,6 +25,7 @@ public class modelCraps {
         dado1=new Dado();
         dado2=new Dado();
         caras=new int[2];
+        estadoToString=new String[2];
         flag=0;
     }
 
@@ -50,12 +63,16 @@ public class modelCraps {
         if(tiro==punto){
             estado=4;
             flag=0;
+        }else{
+
+            if (tiro==7){
+                estado=5;
+                flag=0;
+            }else{
+                estado=6;
+            }
         }
 
-        if (tiro==7){
-            estado=5;
-            flag=0;
-        }
     }
 
     public int getTiro() {
@@ -66,24 +83,41 @@ public class modelCraps {
         return punto;
     }
 
-    public String getEstadoToString() {
+    /**
+     * Estabilish message game state according to estado atribute value
+     * @return message  for the view class
+     */
+    public String[] getEstadoToString() {
         switch (estado){
             case 1:
-                estadoToString="Sacaste natural, Has ganado!";
+                estadoToString[0]="Tiro de salida= "+tiro;
+                estadoToString[1]="Sacaste natural, Has ganado!";
                 break;
             case 2:
-                estadoToString="Sacaste craps, Has perdido!!";
+                estadoToString[0]="Tiro de salida= "+tiro;
+                estadoToString[1]="Sacaste craps, Has perdido!!";
                 break;
             case 3:
-                estadoToString="Estableciste punto en"+punto+
+                estadoToString[0]="Tiro de salida= "+tiro+ "'\nPunto = "+ punto;
+                estadoToString[1]="Estableciste punto en"+punto+
                 "Debes seguir lanzando!!"+
-                "\n pero si haces sacas 7 antes que "+punto+"perderas";
+                "\n pero si  sacas 7 antes que "+punto+" perderas";
                 break;
             case 4:
-                estadoToString="Volviste a sacar"+punto+", has ganado!!";
+                estadoToString[0]="Tiro de salida= "+tiro+ "'\nPunto = "+ punto
+                +"\nValor del nuevo tiro = "+ tiro;
+                estadoToString[1]="Volviste a sacar"+punto+", has ganado!!";
                 break;
                 case 5:
-                estadoToString="Sacaste 7 antes que "+punto+"Has perdido!!";
+                    estadoToString[0]="Tiro de salida= "+tiro+ "'\nPunto = "+ punto+
+                    "\nValor del nuevo tiro= "+tiro;
+                estadoToString[1]="Sacaste 7 antes que "+punto+"Has perdido!!";
+                break;
+            case 6:
+                estadoToString[0]="Tiro de salida= "+punto+ "'\nPunto = "+ punto+
+                        "\nValor del nuevo tiro= "+tiro;
+                estadoToString[1]= "Estas en punto y Debes seguir lanzando!!"+
+                        "\n pero si  sacas 7 antes que "+punto+" perderas";;
                 break;
 
         }
@@ -91,6 +125,7 @@ public class modelCraps {
     }
 
     public int[] getCaras() {
+
         return caras;
     }
 }
